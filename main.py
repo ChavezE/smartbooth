6be390357c8e7135f362924204d2a755cd539f0c
Fabian Mejia <fabian.mejia91@gmail.com>
2018-09-27 16:09:44 -0500

    se cambiaron las views

---
+++ main.py
@@ -0,0 +1,95 @@
+#main.py --pyqt5
+#mainwindow.ui
+#pushbutton
+#text1
+
+import sys
+
+from PyQt5.QtCore import (pyqtSlot, pyqtSignal)
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from registerWindow import registerWindow
+from PyQt5.uic import loadUi
+from PyQt5.QtGui import QMovie
+from cardReader import scanForRDIF
+
+import pyrebase
+
+import threading 
+
+# Module 2
+sys.path.insert(0, '../Module_2')
+from dummy_endpoint import PhotoMain
+
+config = {
+	"apiKey": "AIzaSyAq9xA-sjwtOmye3j_xzURxacHP6qknLOg",
+	"authDomain": "photoboot-e2b33.firebaseapp.com",
+	"databaseURL": "https://photoboot-e2b33.firebaseio.com",
+	"storageBucket": "photoboot-e2b33.appspot.com",
+}
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+
+def userExists(rfid):
+	mat = 0
+	students = db.child("Students").order_by_child("RFID").equal_to(int(rfid)).get()
+	try:
+		students_values = students.val()
+
+		for i in students_values:
+			# this retrieves the key
+			mat = i
+
+		print ("User does exists")
+		return True, mat
+
+	except IndexError:
+		print ("User does not exists")
+		return False, mat
+
+class Main(QMainWindow):
+
+	scanRDIFE = pyqtSignal(str, name="scanRDIFE")
+
+	def __init__(self):
+		super(Main, self).__init__()
+		loadUi("../UserExperience/views/mainwindow.ui", self)
+
+		movie = QMovie ("../UserExperience/img/welcome.gif")
+		self.rfid = 0
+		self.label.setMovie(movie)
+		movie.start()
+		self.scanRDIFE.connect(self.onSuccessRead)
+
+	@pyqtSlot(str)
+	def onSuccessRead(self, id):
+		print(id)
+		check, mat = userExists(id)
+		if (not(check)):
+			register = registerWindow(self)
+			register.exec_()
+		else:
+			module2 = PhotoMain(self, mat)
+			module2.exec_()
+
+	def scanRfid(self):
+		print("scan")
+		self.rfid, text = scanForRDIF()
+		print("readed")
+		self.scanRDIFE.emit(str(self.rfid))
+		print("emited")
+
+	def initialize_reader(self):
+		threading.Thread(target=main.scanRfid).start()
+		
+
+
+app = QApplication(sys.argv)
+main = Main()
+main.show()
+main.initialize_reader()
+
+sys.exit(app.exec_())
+
+
+#py main.py
---
+++ registerWindow.py
@@ -0,0 +1,67 @@
+from PyQt5.QtWidgets import QDialog
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.uic import loadUi
+from validation import validateMatricule, validateCareer
+import sys
+import pyrebase
+import threading 
+
+# Module 2
+sys.path.insert(0, '../Module_2')
+from dummy_endpoint import PhotoMain
+
+config = {
+    "apiKey": "AIzaSyAq9xA-sjwtOmye3j_xzURxacHP6qknLOg",
+    "authDomain": "photoboot-e2b33.firebaseapp.com",
+    "databaseURL": "https://photoboot-e2b33.firebaseio.com",
+    "storageBucket": "photoboot-e2b33.appspot.com",
+}
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+
+class registerWindow(QDialog):
+	def __init__(self, parent):
+		super(registerWindow, self).__init__()
+		loadUi("register.ui", self)
+		self.globalParent = parent
+		self.RFID = parent.rfid
+		print(parent.rfid)
+		self.registerButton.clicked.connect(self.registerUsers)
+		parent.hide()
+
+	def initialize_reader(self):
+		self.globalParent.show()
+		threading.Thread(target=self.globalParent.scanRfid).start()
+
+	@pyqtSlot()
+	def registerUsers(self):
+		print("register")
+		print(self.RFID)
+		self.mat_error.setText("")
+		self.car_error.setText("")
+		print(self.studentId.text())
+		print(self.carrera.text())
+		mat = self.studentId.text()
+		career = self.carrera.text()
+		print("Matricula: ",validateMatricule(mat))
+		print("Carrera: ",validateCareer(career))
+		
+		# Call validation
+		validateMat = validateMatricule(mat)
+		validateCar = validateCareer(career)
+		if(validateMat and validateCar):
+			data = {"RFID": self.RFID,  "carrera": career, "matricula": mat}
+			
+			db.child("Students").child(mat).set(data)
+
+			# Call module 2 after registration
+			print (self.studentId)
+			module2 = PhotoMain(self, mat)
+			module2.exec_()
+		else:
+			if(not(validateMat)):
+				self.mat_error.setText("Matrícula inválida")
+			if(not(validateCar)):
+				self.car_error.setText("Carrera inválida")
+		
